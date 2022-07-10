71f1204ae4fc99165b88df1e314dbee77efea144
lcsrodriguez <lucasrodriguez.08@outlook.com>
2022-07-11 00:19:08 +0200

    Adding HDView logic and declaration of HDGroup

--- headat/main.py
+++ headat/main.py
@@ -6,12 +6,44 @@ class HDView():
     """
     Main class 
     """
+    VIEWS_INITIALIZED_COUNTER = 0
+    VIEWS_TITLES = []
 
-    def __init__(self) -> None:
+    def __init__(self, source: str = None, title: str = None) -> None:
         """
-        Constructor
+        Constructor function initializing a new HDView object
         """
-        print("Salut")
+
+        # Parsing the arguments of the c-tor
+        self.source = source
+        self.title = title if title != None else f"HDView #{HDView.nb_views_initialized}"
+
+        # Increment the number of initialized views in order to get a count
+        HDView.VIEWS_INITIALIZED_COUNTER += 1
+        HDView.VIEWS_TITLES.append(self.title)
+
+        print("Logging the creation of a new HDView")
+
+    def add_record(self):
+        """
+        Function allowing user to add a record to the view
+        """
+        if self.source != None:
+            # TODO : Add folder check
+            # TODO : Add if there is a .hea and a corresponding .dat file
+            pass
+
+    def convert_record(self, format_type: str = None):
+        """
+        Function allowing 
+        """
+
+
+class HDGroup():
+    """
+    HDGroup class
+    """
+    pass
 
 
 """
